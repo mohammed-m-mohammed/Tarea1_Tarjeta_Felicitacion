5dcf1da1f1dd1a43bf756c963da805ee00c37794
Mudi <mohammed.mustafa.infor@gmail.com>
2019-03-02 21:19:23 +0100

    version final

--- app/src/main/java/com/itt/mohammed/tarea1_felicitacionnavidenia/MainActivity.java
+++ app/src/main/java/com/itt/mohammed/tarea1_felicitacionnavidenia/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import static android.media.MediaPlayer.*;
@@ -17,7 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         Typeface miFuente = Typeface.createFromAsset(getAssets(), "HappyBirthday.ttf");
         TextView titulo = (TextView) findViewById(R.id.titulo);
+        TextView texto2 = (TextView) findViewById(R.id.textView2);
+        TextView texto3 = (TextView) findViewById(R.id.textView3);
+
         titulo.setTypeface(miFuente);
+
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        titulo.startAnimation(animacion);
+
+        Animation slideUp = AnimationUtils.loadAnimation(this, R.anim.slideup);
+        texto2.startAnimation(slideUp);
+        texto3.startAnimation(slideUp);
 
         MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.birthday_song);
         mediaPlayer.start();
