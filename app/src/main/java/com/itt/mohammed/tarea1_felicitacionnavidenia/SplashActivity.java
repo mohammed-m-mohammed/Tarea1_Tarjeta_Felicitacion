4b28f687b397e2bfa8fdaa0582542199a9e3cc6a
Mudi <mohammed.mustafa.infor@gmail.com>
2019-03-02 22:02:43 +0100

    version final

---
+++ app/src/main/java/com/itt/mohammed/tarea1_felicitacionnavidenia/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.itt.mohammed.tarea1_felicitacionnavidenia;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        Typeface myFont = Typeface.createFromAsset(getAssets(), "TimeToParty.ttf");
+        TextView texto = (TextView) findViewById(R.id.textoSplash);
+
+        texto.setTypeface(myFont);
+
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.splash);
+        texto.startAnimation(animacion);
+
+        animacion.setAnimationListener(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
